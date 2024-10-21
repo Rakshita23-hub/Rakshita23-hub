e417100638da6ab20b7105d713771275fa2618cd
Rakshita23-hub <rrhalawai@gmail.com>
2024-10-21 15:28:43 +0530

    added a new file

--- prog1.c
+++ prog1.c
@@ -2,7 +2,11 @@
 void main()
 {
 char name;
+int age;
+int  phone num;
+printf("nidhiii's phone nummm:%d",phone num);
 printf("Nidhiiiiii");
 scanf("%s", name);
+scanf("%d",&phone num);
 }
-
+//git lab
