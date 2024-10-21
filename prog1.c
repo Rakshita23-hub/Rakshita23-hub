1c41852c5367ec3e49bbb1bcf3c5d3db906e55ad
Rakshita23-hub <rrhalawai@gmail.com>
2024-10-21 15:37:12 +0530

    added lines

--- prog1.c
+++ prog1.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 void main()
 {
+double salary;
 char name;
 int age;
 int  phone num;
 printf("nidhiii's phone nummm:%d",phone num);
 printf("Nidhiiiiii");
+printf("salary:");
 scanf("%s", name);
 scanf("%d",&phone num);
 }
